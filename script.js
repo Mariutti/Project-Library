4c296372dae9ac8b969a46e44f5fee5562f89097
Mariutti <pedromariutti@gmail.com>
2023-12-28 16:20:14 -0300

    feature: function checkbox isReaded

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = bookList
+window.onload = bookList;
 
 const myLibrary = [];
 
@@ -31,22 +31,23 @@ function bookList() {
 	const ulBooks = document.querySelector(".card-container");
 	// ulBooks.innerHTML = ''
 	myLibrary.map((book) => {
-
-		const li = document.createElement('li')
-		li.classList.add('card')
+		const li = document.createElement("li");
+		li.classList.add("card");
 		li.innerHTML = `
 		<h3>Title: ${book.title}</h3>
 		<p>Author: ${book.author}</p>
 		<p>number of pages: ${book.pages}</p>
-		
-		`
+		<label class="check">
+			<span>Already readed?</span>
+			<input type="checkbox" name="isReaded" id="isReaded" class="isReaded" ${book.isReaded? 'checked': ''}>	
+		</label>
+		`;
 
-		ulBooks.appendChild(li)
+		ulBooks.appendChild(li);
 	});
 }
 
-
-const submitBtn = document.querySelector('.submitBtn');
+const submitBtn = document.querySelector(".submitBtn");
 submitBtn.addEventListener("click", addBook);
 
 function addBook(event) {
@@ -55,9 +56,18 @@ function addBook(event) {
 	const bookAuthor = document.getElementById("bookAuthor");
 	const bookPages = document.getElementById("bookPages");
 
-	const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value)
+	const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
 
-	addBookToLibrary(newBook)
-	
+	addBookToLibrary(newBook);
+
 	bookList();
+}
+
+const checkBtn = document.querySelectorAll('.isReaded')
+checkBtn.forEach(btn => {
+	btn.addEventListener('click', checkIsReaded)
+})
+
+function checkIsReaded(e){
+	console.log(e.target)
 }
